ba8fc1cef6696261bc3005a9579fdb99a039de48
ksoliven <kate.c.soliven@gmail.com>
2019-07-10 08:21:48 -0400

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using DojoSurvey.Models;
+namespace DojoSurvey.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Requests
+        // localhost: 5000/
+        // [Route("")]
+        [HttpGet("")]
+
+        public IActionResult Index()
+        {
+            //ViewResult myView = View();
+            return View("Index");
+        }
+        [HttpPost("Result")]
+        public IActionResult Result(Survey yourSurvey) 
+        {  
+            return View("Result", yourSurvey);
+        }
+    }
+}
---
+++ Models/Home/User.cs
@@ -0,0 +1,10 @@
+namespace DojoSurvey.Models
+{
+    public class Survey
+    {
+        public string name {get; set;}
+        public string dojo {get; set;}
+        public string lang {get; set;}
+        public string comment {get; set;}
+    }
+}
